dfb5e4eac24bafb6202f53b2d4012f2b5a518fd3
Mayrin <mayrin.peredia@cetys.edu.mx>
2026-04-18 21:39:42 -0700

    Codigo corregido

--- frontend/app/(tabs)/HomeScreen.tsx
+++ frontend/app/(tabs)/HomeScreen.tsx
@@ -1,19 +1,13 @@
 import '../../global.css';
 import { Button } from '@/components/ui/Button';
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Borel&display=swap');
-</style>
-
 export default function HomeScreen() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-[#FAFAFA] via-[#30C2D9] to-[#FF9B42]">
-        <h1 className="text-4xl font-bold mb-40 font">FriendFrame</h1>
+      <h1 className="text-4xl font-bold mb-40 font-borel">FriendFrame</h1>
 
-        <Button title="Get Started" onPress={() => {}} variant='secondary' />
-
-        <Button title="Have an Account?" onPress={() => {}} variant='primary'/>
-
+      <Button title="Get Started" onPress={() => {}} variant="secondary" />
+      <Button title="Have an Account?" onPress={() => {}} variant="primary" />
     </div>
   );
 }
--- frontend/components/ui/Button.tsx
+++ frontend/components/ui/Button.tsx
@@ -14,21 +14,16 @@ export function Button({ title, onPress, variant = "primary" }: Props) {
       className={`w-[214px] py-3 rounded-full items-center
         ${variant === "primary" 
           ? "bg-[#1D2A4F]" 
-          : "bg-[#E5E7EB] border-2 border-[#1D2A4F]"} mb-4 mt-3"
-        }
-      `}
+          : "bg-[#E5E7EB] border-2 border-[#1D2A4F]"} mb-4 mt-3`}
     >
       <Text
         className={`font-semibold text-base
           ${variant === "primary" 
             ? "text-white" 
-            : "text-[#1D2A4F]"
-          }
-        `}
+            : "text-[#1D2A4F]"}`}
       >
         {title}
       </Text>
-      
     </TouchableOpacity>
   );
 }
