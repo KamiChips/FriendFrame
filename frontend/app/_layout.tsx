5a01264d79997c2258a996762fb6892e8919f20f
Christopher-Cetys <dsandez@cetys.edu.mx>
2026-04-22 01:48:41 -0700

    feat: implementar SplashScreen responsivo multiplataforma por si acaso

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,8 +2,10 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { useEffect, useState } from 'react';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import SplashScreen from '@/components/ui/SplashScreen'; 
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,7 +13,23 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [appIsReady, setAppIsReady] = useState(false);
 
+  useEffect(() => {
+    //Simulador de carga de 8 segundos
+    const timer = setTimeout(() => {
+      setAppIsReady(true);
+    }, 8000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Mostramos splash mientras la app "carga"
+  if (!appIsReady) {
+    return <SplashScreen />;
+  }
+
+  // cuando carga, renderizamos la navegación de los temas
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
