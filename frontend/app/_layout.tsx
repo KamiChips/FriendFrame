6c40cc680a9dc92989820ea9bc9adf3e1631f66d
Christopher-Cetys <dsandez@cetys.edu.mx>
2026-04-22 01:48:41 -0700

    feat: implementar SplashScreen responsivo multiplataforma por si acaso

---
+++ frontend/app/_layout.tsx
@@ -0,0 +1,41 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+import { useEffect, useState } from 'react';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import SplashScreen from '@/components/ui/SplashScreen'; 
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    //Simulador de carga de 8 segundos
+    const timer = setTimeout(() => {
+      setAppIsReady(true);
+    }, 8000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Mostramos splash mientras la app "carga"
+  if (!appIsReady) {
+    return <SplashScreen />;
+  }
+
+  // cuando carga, renderizamos la navegación de los temas
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
