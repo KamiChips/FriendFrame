5fc4ecface5b5def7ff44f402f6ff02a40131c59
Mayrin <mayrin.peredia@cetys.edu.mx>
2026-04-27 22:28:00 -0700

    Fixed issue with inability to view on mobile

--- frontend/app/(tabs)/HomeScreen.tsx
+++ frontend/app/(tabs)/HomeScreen.tsx
@@ -1,13 +1,40 @@
+import { Link } from 'expo-router';
 import '../../global.css';
 import { Button } from '@/components/ui/Button';
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { View, Text, useColorScheme } from 'react-native';
 
 export default function HomeScreen() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-[#FAFAFA] via-[#30C2D9] to-[#FF9B42]">
-      <h1 className="text-4xl font-bold mb-40 font-borel">FriendFrame</h1>
+  const isDark = useColorScheme() === 'dark';
 
-      <Button title="Get Started" onPress={() => {}} variant="secondary" />
-      <Button title="Have an Account?" onPress={() => {}} variant="primary" />
-    </div>
+  return (
+    <LinearGradient
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+      className={`px-6 ${isDark ? 'dark' : ''}`}
+      colors={
+        isDark ? ["#182240", "#115A67", "#AA3E14"]
+          : ["#FAFAFA", "#30C2D9", "#FF9B42"]
+      }
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
+
+      <View className="mb-20">
+        <Text className="text-4xl mb-40 font-borel">
+          FriendFrame
+        </Text>
+
+        <Link href="/SignupScreen" asChild>
+          <Button title="Get Started" variant="secondary" onPress={function (): void {
+          }} />
+        </Link>
+
+        <Link href="/LoginScreen" asChild>
+          <Button title="Have an Account?" onPress={() => { }} variant="primary" />
+        </Link>
+      </View>
+
+    </LinearGradient>
   );
 }
