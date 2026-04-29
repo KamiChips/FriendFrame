24608ff655fb1e5ed2e7cec7811fed17f35d347a
ElNito7 <kelo.camachoromero@gmail.com>
2026-04-29 13:39:02 -0700

    ChatInboxScreen dark mode and unread mark

--- frontend/app/(tabs)/ChatInboxScreen.tsx
+++ frontend/app/(tabs)/ChatInboxScreen.tsx
@@ -1,51 +1,48 @@
 import { ScrollView, StatusBar, Text, Image, View, useColorScheme, TouchableOpacity } from "react-native";
 import '../../global.css';
 import ChatCard from "@/components/ui/ChatCard";
+import HeaderWithLogoAndNotificationBell from "@/components/ui/HeaderWithLogoAndNotificationBell";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ChatInboxScreen = () => {
 
     const colorScheme = useColorScheme();
-    const isDark = false;
-    // colorScheme === 'dark';
+    const isDark = colorScheme === 'dark';
 
     // chats de prueba
     const CHATS = [
-    {
-        id: 1,
-        initials: 'CR',
-        name: 'Carlos Ramírez',
-        message: 'Claro! Nos vemos mañana',
-        time: 'hace 10 min',
-        unread: true,
-    },
-    {
-        id: 2,
-        initials: 'AL',
-        name: 'Ana López',
-        message: 'Gracias por la recomendación!',
-        time: 'hace 2 horas',
-        unread: false,
-    },
-];
+        {
+            id: 1,
+            initials: 'CR',
+            name: 'Carlos Ramírez',
+            message: 'Claro! Nos vemos mañana',
+            time: 'hace 10 min',
+            unread: true,
+        },
+        {
+            id: 2,
+            initials: 'AL',
+            name: 'Ana López',
+            message: 'Gracias por la recomendación!',
+            time: 'hace 2 horas',
+            unread: false,
+        },
+    ];
 
     return (
-        <View className="flex-1 bg-background-gray mt-2">
-            <StatusBar barStyle="dark-content" backgroundColor="#FFFFFF" />
+        <SafeAreaView className={`flex-1 bg-background-gray ${isDark ? 'dark' : ''} dark:bg-background-semidark `} >
+            <StatusBar 
+                barStyle={isDark ? 'light-content' : 'dark-content'} 
+                backgroundColor={isDark ? '#1a1a1a' : "#ffffff"}
+            />
 
             {/* Header */}
-            <View className="flex-row items-center justify-between bg-background-light px-5 pb-3 pt-4">
-                <Text className="font-bold text-2xl italic tracking-wider">FriendFrame</Text>
-                <TouchableOpacity>
-                    <Image 
-                        source={require('../../assets/images/notification-bell.png')} 
-                    />
-                </TouchableOpacity>
-            </View>
+            <HeaderWithLogoAndNotificationBell isDark={isDark} />
 
             {/* Inbox */}
-            <View className="flex-1 bg-gray-50">
+            <View className="flex-1 bg-gray-50 dark:bg-background-dark">
                 <View className="px-4 pt-5 pb-3">
-                    <Text className="font-bold text-2xl text-[#1a1a1a]">
+                    <Text className="font-bold text-2xl text-[#1a1a1a] dark:text-background-light">
                         Mensajes
                     </Text>
                 </View>
@@ -56,12 +53,12 @@ const ChatInboxScreen = () => {
                 >
                     {/* Chats */}
                     {CHATS.map(chat => (
-                        <ChatCard key={chat.id} {...chat} />
+                        <ChatCard key={chat.id} {...chat} isDark={isDark} />
                     ))}
                 </ScrollView>
             </View>
-        </View>
-    )
+        </SafeAreaView>
+    );
 }
 
-export default ChatInboxScreen
+export default ChatInboxScreen;
