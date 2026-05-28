020d04132c005f678aca5224de6ccb0d53e998bd
Mayrin <mayrin.peredia@cetys.edu.mx>
2026-05-27 21:10:21 -0700

    Cambiar header de sign y log a transparente

--- frontend/app/(auth)/LoginScreen.tsx
+++ frontend/app/(auth)/LoginScreen.tsx
@@ -4,6 +4,7 @@ import { View, Text, TouchableOpacity, Image, useColorScheme } from 'react-nativ
 import '../../global.css';
 import { TextField } from '../../components/ui/TextField';
 import { Button } from '../../components/ui/Button';
+import { router, Stack } from 'expo-router';
 
 const LoginScreen = () => {
 
@@ -22,77 +23,80 @@ const LoginScreen = () => {
     }
 
     return (
-        <LinearGradient
-            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-            className={`px-6 ${isDark ? 'dark' : ''}`}
-            colors={
-                isDark ? ["#182240", "#115A67", "#AA3E14"] 
-                : ["#FAFAFA", "#30C2D9", "#FF9B42"]
-            }
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
-        >
-            <View className='w-3/4 self-center'>
-                {/* Welcome back */}
-                <View className="mb-20">
-                    <Text className="text-background-dark dark:text-background-light text-4xl font-bold">
-                    Welcome Back!
-                    </Text>
-                    <Text className="text-background-dark dark:text-background-light text-xl">
-                    See what your friends shared about you
-                    </Text>
-                </View>
-
-                {/* Login form */}
-                <View className="mb-18">
-                    <TextField
-                        placeholder="Username"
-                        value={username}
-                        onChangeText={setUsername}
-                    />
-
-                    <TextField
-                        placeholder="Password"
-                        value={password}
-                        onChangeText={setPassword}
-                        secureTextEntry
-                    />
-
-                    {/* Remember me / Forgot Password? */}
-                    <View className="flex-row justify-between mb-12">
-                        <Text className='dark:text-background-light'>[] Remember me</Text>
-                        <Text className='font-semibold dark:text-background-light'>Forgot Password?</Text>
-                    </View>
-
-                    <View className='items-center mb-18 mt-12'>
-                        <Button 
-                            title="Log In" 
-                            variant={isDark ? 'primary' : 'secondary'} 
-                            onPress={handleSubmit} 
-                        />
-                    </View>
-                </View>
-
-                {/* Login with Google or Sign up */}
-                <View className='items-center mb-4 mt-8'>
-                    <Text className='dark:text-background-light mb-4'>
-                        Log in with
-                    </Text>
-                    <TouchableOpacity onPress={logInWithGoogle} >
-                        {/* Google Logo */}
-                        <Image 
-                            source={require('../../assets/images/google-logo.png')}
-                        />
-                    </TouchableOpacity>
-                    <Text className='dark:text-background-light mt-4'>
-                        Don't have an account? {" "}
-                        <Text className='font-semibold dark:text-background-light' onPress={() => console.log("go to sign up screen")}>
-                            Sign up
+        <>
+            <Stack.Screen
+                options={{
+                    headerTransparent: true,
+                    headerTitle: "",
+                    headerTintColor: isDark ? '#FAFAFA' : '#182240',
+                }}
+            />
+            
+            <LinearGradient
+                style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+                className={`px-6 ${isDark ? 'dark' : ''}`}
+                colors={isDark ? ["#182240", "#115A67", "#AA3E14"]
+                    : ["#FAFAFA", "#30C2D9", "#FF9B42"]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+            >
+                <View className='w-3/4 self-center'>
+                    {/* Welcome back */}
+                    <View className="mb-20">
+                        <Text className="text-background-dark dark:text-background-light text-4xl font-bold">
+                            Welcome Back!
                         </Text>
-                    </Text>
+                        <Text className="text-background-dark dark:text-background-light text-xl">
+                            See what your friends shared about you
+                        </Text>
+                    </View>
+
+                    {/* Login form */}
+                    <View className="mb-18">
+                        <TextField
+                            placeholder="Username"
+                            value={username}
+                            onChangeText={setUsername} />
+
+                        <TextField
+                            placeholder="Password"
+                            value={password}
+                            onChangeText={setPassword}
+                            secureTextEntry />
+
+                        {/* Remember me / Forgot Password? */}
+                        <View className="flex-row justify-between mb-12">
+                            <Text className='dark:text-background-light'>[] Remember me</Text>
+                            <Text className='font-semibold dark:text-background-light'>Forgot Password?</Text>
+                        </View>
+
+                        <View className='items-center mb-18 mt-12'>
+                            <Button
+                                title="Log In"
+                                variant={isDark ? 'primary' : 'secondary'}
+                                onPress={handleSubmit} />
+                        </View>
+                    </View>
+
+                    {/* Login with Google or Sign up */}
+                    <View className='items-center mb-4 mt-8'>
+                        <Text className='dark:text-background-light mb-4'>
+                            Log in with
+                        </Text>
+                        <TouchableOpacity onPress={logInWithGoogle}>
+                            {/* Google Logo */}
+                            <Image
+                                source={require('../../assets/images/google-logo.png')} />
+                        </TouchableOpacity>
+                        <Text className='dark:text-background-light mt-4'>
+                            Don't have an account? {" "}
+                            <Text className='font-semibold dark:text-background-light' onPress={() => router.push('/SignupScreen')}>
+                                Sign up
+                            </Text>
+                        </Text>
+                    </View>
                 </View>
-            </View>
-        </LinearGradient>
+            </LinearGradient></>
     ) 
 }
 
